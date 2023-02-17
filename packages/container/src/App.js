df4d4ef2d11fe54c6178fcfba51a4863b643805e
Maxim Didenko <didenko.mn@gmail.com>
2023-02-17 14:46:40 +0200

    configured css in js for marketing and container apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ct',
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
         <Header />
         <MarketingApp />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
